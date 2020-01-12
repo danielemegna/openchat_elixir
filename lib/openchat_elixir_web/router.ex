42d3cb0e60d7fce84d29109bf20a1bc09de13cb4
Daniele Megna <megna.dany@gmail.com>
2020-01-12 11:00:31 +0100

    Remove unecessary api pipeline in router

--- lib/openchat_elixir_web/router.ex
+++ lib/openchat_elixir_web/router.ex
@@ -1,11 +1,5 @@
 defmodule OpenchatElixirWeb.Router do
   use OpenchatElixirWeb, :router
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
-  pipe_through :api
-  
   get "/users", OpenchatElixirWeb.UserController, :get_all
 end
