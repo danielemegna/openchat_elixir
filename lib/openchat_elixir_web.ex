d5d82ede264bab95ce9bc465c2b27a35d1e783e4
Daniele Megna <daniele.megna@xpeppers.com>
2020-02-25 09:14:18 +0100

    Removed unused elixir stuff

--- lib/openchat_elixir_web.ex
+++ lib/openchat_elixir_web.ex
@@ -22,25 +22,6 @@ defmodule OpenchatElixirWeb do
       use Phoenix.Controller, namespace: OpenchatElixirWeb
 
       import Plug.Conn
-      import OpenchatElixirWeb.Gettext
-      alias OpenchatElixirWeb.Router.Helpers, as: Routes
-    end
-  end
-
-  def view do
-    quote do
-      use Phoenix.View,
-        root: "lib/openchat_elixir_web/templates",
-        namespace: OpenchatElixirWeb
-
-      # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
-
-      # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
-
-      import OpenchatElixirWeb.ErrorHelpers
-      import OpenchatElixirWeb.Gettext
       alias OpenchatElixirWeb.Router.Helpers, as: Routes
     end
   end
@@ -50,13 +31,6 @@ defmodule OpenchatElixirWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
-    end
-  end
-
-  def channel do
-    quote do
-      use Phoenix.Channel
-      import OpenchatElixirWeb.Gettext
     end
   end
 
