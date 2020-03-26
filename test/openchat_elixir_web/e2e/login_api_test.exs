e9248dd40525bad1682e537f87a6c465506729a4
Daniele Megna <daniele.megna@xpeppers.com>
2020-03-26 09:12:09 +0100

    Add skipped register and login user e2e test

--- test/openchat_elixir_web/e2e/login_api_test.exs
+++ test/openchat_elixir_web/e2e/login_api_test.exs
@@ -10,4 +10,26 @@ defmodule OpenchatElixirWeb.E2E.LoginApiTest do
     assert response_text == "Invalid credentials."
   end
 
+  @tag :skip
+  test "register and login a user", %{conn: conn} do
+    conn = post(conn, "/users", %{
+      username: "shady90",
+      password: "s3cure",
+      about: "About shady90."
+    })
+    response_body = json_response(conn, 201) 
+    shady90_id = response_body["id"]
+
+    conn = post(conn, "/login", %{
+      username: "shady90",
+      password: "s3cure"
+    })
+    response_body = json_response(conn, 200)
+    assert response_body == %{
+      "id" => shady90_id,
+      "username" => "shady90",
+      "about" => "About shady90."
+    }
+  end
+
 end
