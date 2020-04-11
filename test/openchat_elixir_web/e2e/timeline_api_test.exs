837d45077a980797a7d3abb700fc490673ad82ed
Daniele Megna <megna.dany@gmail.com>
2020-04-11 13:12:35 +0200

    Add skipped next E2E behaviour to be verified

--- test/openchat_elixir_web/e2e/timeline_api_test.exs
+++ test/openchat_elixir_web/e2e/timeline_api_test.exs
@@ -21,6 +21,7 @@ defmodule OpenchatElixirWeb.E2E.TimelineApiTest do
     assert text_response(conn, 404) == "User not found."
   end
 
+  @tag :skip
   test "submit post and fetch user timeline", %{conn: conn} do
     conn = post(conn, "/users", %{ username: "any", password: "any", about: "any" })
     user_id = json_response(conn, 201)["id"]
@@ -34,6 +35,16 @@ defmodule OpenchatElixirWeb.E2E.TimelineApiTest do
     assert user_id == response_body["userId"]
     assert "First shady90 post." == response_body["text"]
     assert Regex.match?(~r/^((19|20)[0-9][0-9])[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])[T]([01][0-9]|[2][0-3])[:]([0-5][0-9])[:]([0-5][0-9])Z$/i, response_body["dateTime"])
+
+    conn = get(conn, "/users/#{user_id}/timeline")
+    assert json_response(conn, 200) == [
+      %{
+        postId: response_body["postId"],
+        userId: user_id,
+        text: "First shady90 post.",
+        dateTime: response_body["dateTime"]
+      }
+    ]
   end
 
 end
