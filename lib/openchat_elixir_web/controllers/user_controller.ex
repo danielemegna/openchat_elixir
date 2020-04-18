5139348453e1a5126d7b83e74f410d548ead97aa
Daniele Megna <megna.dany@gmail.com>
2020-04-18 11:54:53 +0200

    Refactoring in UserController: extract serialize_user

--- lib/openchat_elixir_web/controllers/user_controller.ex
+++ lib/openchat_elixir_web/controllers/user_controller.ex
@@ -5,13 +5,7 @@ defmodule OpenchatElixirWeb.UserController do
   def get_all(conn, _params) do
     users = GetAllUsersCommand.run()
 
-    json(put_status(conn, :ok), Enum.map(users, fn(user) ->
-      %{
-        id: user.id,
-        username: user.username,
-        about: user.about
-      }
-    end))
+    json(put_status(conn, :ok), Enum.map(users, &serialize_user/1))
   end
 
   def register(conn, params) do
@@ -23,16 +17,19 @@ defmodule OpenchatElixirWeb.UserController do
 
     case(RegisterUserCommand.run(user)) do
       {:ok, created_user} ->
-        json(put_status(conn, :created), %{
-          id: created_user.id,
-          username: created_user.username,
-          about: created_user.about
-        })
+        json(put_status(conn, :created), serialize_user(created_user))
       {:username_already_used, _} ->
         text(put_status(conn, :bad_request), "Username already in use.")
     end
 
   end
 
+  defp serialize_user(user) do
+    %{
+      id: user.id,
+      username: user.username,
+      about: user.about
+    }
+  end
 
 end
