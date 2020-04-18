e12beb6d3f00c8f4b0ae6984416a03dc86952cc4
Daniele Megna <megna.dany@gmail.com>
2020-04-18 09:10:08 +0200

    Use PostRepository in GetUserTimelineCommand to fetch user posts

--- lib/openchat_elixir/commands/get_user_timeline.ex
+++ lib/openchat_elixir/commands/get_user_timeline.ex
@@ -1,11 +1,12 @@
 defmodule OpenchatElixir.GetUserTimelineCommand do
   alias OpenchatElixir.AgentUserRepository
+  alias OpenchatElixir.AgentPostRepository
 
-  def run(user_id, user_repository \\ AgentUserRepository) do
+  def run(user_id, user_repository \\ AgentUserRepository, post_repository \\ AgentPostRepository) do
     user = user_repository.get_by_id(user_id)
     case(user) do
       nil -> {:user_not_found, nil}
-      _ -> {:ok, []}
+      _ -> {:ok, post_repository.get_by_userid(user_id)}
     end
   end
 
--- test/openchat_elixir/commands/get_user_timeline_test.exs
+++ test/openchat_elixir/commands/get_user_timeline_test.exs
@@ -1,34 +1,54 @@
 defmodule OpenchatElixirWeb.GetUserTimelineCommandTest do
   alias OpenchatElixir.GetUserTimelineCommand
-  alias OpenchatElixir.Entities.User
+  alias OpenchatElixir.Entities.{User,Post}
 
   use ExUnit.Case 
   import Mox
 
   setup do
-    stub(MockUserRepository, :get_by_id, fn _ -> nil end)
+    stub(MockUserRepository, :get_by_id, fn user_id -> case user_id do
+      "registered.user.id" -> %User{ id: user_id, username: "any", password: "any", about: "any" }
+      _ -> nil
+    end end)
+    stub(MockPostRepository, :get_by_userid, fn _ -> [] end)
     :ok
   end
 
   test "user not found error" do
-    {result, posts} = GetUserTimelineCommand.run("any", MockUserRepository)
+    {result, posts} = GetUserTimelineCommand.run("not.registered", MockUserRepository, MockPostRepository)
 
     assert :user_not_found == result
     assert nil == posts 
   end
 
   test "user without posts" do
-    stored_user = %User{
-      id: "stored-user-id",
-      username: "stored.user",
-      password: "any",
-      about: "any"
-    }
-    expect(MockUserRepository, :get_by_id, 1, fn "stored-user-id" -> stored_user end)
-    {result, posts} = GetUserTimelineCommand.run("stored-user-id", MockUserRepository)
+    {result, posts} = GetUserTimelineCommand.run("registered.user.id", MockUserRepository, MockPostRepository)
 
     assert :ok == result
     assert [] == posts 
+  end
+
+  test "return posts stored in repository" do
+    stored_user_posts = [
+      %Post{
+        id: "an-id",
+        user_id: "registered.user.id", 
+        text: "First post text.", 
+        datetime: DateTime.utc_now() 
+      },
+      %Post{
+        id: "another-id",
+        user_id: "registered.user.id", 
+        text: "Second post text.", 
+        datetime: DateTime.utc_now() 
+      }
+    ]
+    expect(MockPostRepository, :get_by_userid, 1, fn "registered.user.id" -> stored_user_posts end)
+
+    {result, posts} = GetUserTimelineCommand.run("registered.user.id", MockUserRepository, MockPostRepository)
+
+    assert :ok == result
+    assert stored_user_posts == posts 
   end
 
 end
