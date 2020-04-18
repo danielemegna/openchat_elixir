d60d3e356539b2a56217c3fb03132654cd28afa6
Daniele Megna <megna.dany@gmail.com>
2020-04-18 12:02:57 +0200

    Improve and complete TimelineApi E2E test with multiple posts

--- test/openchat_elixir_web/e2e/timeline_api_test.exs
+++ test/openchat_elixir_web/e2e/timeline_api_test.exs
@@ -22,26 +22,34 @@ defmodule OpenchatElixirWeb.E2E.TimelineApiTest do
   end
 
   test "submit post and fetch user timeline", %{conn: conn} do
-    conn = post(conn, "/users", %{ username: "any", password: "any", about: "any" })
+    conn = post(conn, "/users", %{ username: "shady90", password: "any", about: "any" })
     user_id = json_response(conn, 201)["id"]
 
-    conn = post(conn, "/users/#{user_id}/timeline", %{
-      text: "First shady90 post."
-    })
+    conn = post(conn, "/users/#{user_id}/timeline", %{ text: "First shady90 post." })
     response_body = json_response(conn, 201) 
     assert Enum.count(Map.keys(response_body)) == 4
     assert Regex.match?(~r/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i, response_body["postId"])
     assert user_id == response_body["userId"]
     assert "First shady90 post." == response_body["text"]
     assert Regex.match?(~r/^((19|20)[0-9][0-9])[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])[T]([01][0-9]|[2][0-3])[:]([0-5][0-9])[:]([0-5][0-9])Z$/i, response_body["dateTime"])
+    %{ "postId" => firstPostId, "dateTime" => firstPostDateTime } = response_body
+
+    conn = post(conn, "/users/#{user_id}/timeline", %{ text: "Second shady90 post." })
+    %{ "postId" => secondPostId, "dateTime" => secondPostDateTime } = json_response(conn, 201)
 
     conn = get(conn, "/users/#{user_id}/timeline")
     assert json_response(conn, 200) == [
       %{
-        "postId" => response_body["postId"],
+        "postId" => secondPostId,
+        "userId" => user_id,
+        "text" => "Second shady90 post.",
+        "dateTime" => secondPostDateTime
+      },
+      %{
+        "postId" => firstPostId,
         "userId" => user_id,
         "text" => "First shady90 post.",
-        "dateTime" => response_body["dateTime"]
+        "dateTime" => firstPostDateTime
       }
     ]
   end
