0b65df6ed6d69ffa7f90fc1f3f5f44ea204e9690
Daniele Megna <megna.dany@gmail.com>
2020-08-13 17:55:08 +0200

    Refactor in RegisterUserCommandTest :star:

--- test/openchat_elixir/commands/register_user_test.exs
+++ test/openchat_elixir/commands/register_user_test.exs
@@ -12,53 +12,47 @@ defmodule OpenchatElixirWeb.RegisterUserCommandTest do
   end
 
   test "returns stored user with result :ok" do
-    user_to_register = %User{
-      username: "clean.coder90",
-      password: "vEry$ecure",
-      about: "It's all about craft."
-    }
-
-    assert {:ok, registered_user} = RegisterUserCommand.run(user_to_register, MockUserRepository)
+    user_to_register = valid_user()
+    assert {:ok, registered_user} = run_command(user_to_register)
     assert registered_user == %{ user_to_register | id: "stored.user.id" }
   end
 
   test "passed UserRepository module is used to store the user" do
-    user_to_register = %User{
-      username: "clean.coder90",
-      password: "vEry$ecure",
-      about: "It's all about craft."
-    }
+    user_to_register = valid_user()
     expect(MockUserRepository, :store, 1, fn ^user_to_register -> "stored.user.id" end)
 
-    RegisterUserCommand.run(user_to_register, MockUserRepository)
+    run_command(user_to_register)
 
     Mox.verify!
   end
 
   test "register a users twice should return an error" do
-    already_registered_user = %User{
-      username: "stored.user",
-      password: "any",
-      about: "any"
-    }
-    expect(MockUserRepository, :get_by_username, 1, fn "stored.user" -> already_registered_user end)
+    already_registered_user = %User{ valid_user() | username: "already.registered.username" }
+    expect(MockUserRepository, :get_by_username, 1, fn "already.registered.username" -> already_registered_user end)
 
-    assert {:username_already_used, nil} = RegisterUserCommand.run(already_registered_user, MockUserRepository)
+    assert {:username_already_used, nil} = run_command(already_registered_user)
     Mox.verify!
   end
 
   test "username cannot be empty or nil" do
-    user_to_register = %User{ username: "", password: "any", about: "any" }
-    assert {:invalid_user_data, nil} = RegisterUserCommand.run(user_to_register, MockUserRepository)
-    user_to_register = %User{ username: nil, password: "any", about: "any" }
-    assert {:invalid_user_data, nil} = RegisterUserCommand.run(user_to_register, MockUserRepository)
+    assert {:invalid_user_data, nil} = run_command(%User{ valid_user() | username: "" })
+    assert {:invalid_user_data, nil} = run_command(%User{ valid_user() | username: nil })
   end
 
   test "password cannot be empty or nil" do
-    user_to_register = %User{ username: "any", password: "", about: "any" }
-    assert {:invalid_user_data, nil} = RegisterUserCommand.run(user_to_register, MockUserRepository)
-    user_to_register = %User{ username: "any", password: nil, about: "any" }
-    assert {:invalid_user_data, nil} = RegisterUserCommand.run(user_to_register, MockUserRepository)
+    assert {:invalid_user_data, nil} = run_command(%User{ valid_user() | password: "" })
+    assert {:invalid_user_data, nil} = run_command(%User{ valid_user() | password: nil })
   end
+
+  defp run_command(user) do
+    RegisterUserCommand.run(user, MockUserRepository)
+  end
+
+  defp valid_user(), do:
+    %User{
+      username: "clean.coder90",
+      password: "vEry$ecure",
+      about: "It's all about craft."
+    }
 
 end
